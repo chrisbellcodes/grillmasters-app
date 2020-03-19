191565cfff2809df7b8c49154f51c99977c47c2a
github username <github email address>
2020-03-19 15:28:33 -0400

    Connected MongoDB database

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const port = process.env.PORT || 3001;
 app.use(cors()); // Handles CORS
 app.use(express.json()); // parses JSON
 
-
+// Connecting MongDB (via Mongoose)
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log(`Yas - MongoDB connection established succesfully!`);
+})
 
 
 app.listen(port, () => (console.log(`Server is running on port ${port}`)
